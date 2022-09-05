b97ac5ed80a16e3d4214baa3f50ac8424973beaa
Valter Kraemer <valterkraemer@users.noreply.github.com>
2022-09-05 11:01:03 +0300

    Always translate months to en-US for consistency

--- src/lib/months.ts
+++ src/lib/months.ts
@@ -2,7 +2,7 @@ export function monthName(monthIndex: number) {
 	const date = new Date();
 	date.setMonth(monthIndex);
 
-	return date.toLocaleString(undefined, {
+	return date.toLocaleString("en-US", {
 		month: 'long'
 	});
 }
